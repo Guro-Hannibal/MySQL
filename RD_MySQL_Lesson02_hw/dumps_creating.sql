59e04fef11234f2677fc07d4f2548297177b7d2e
Guro <demensdeo@gmail.com>
2022-01-01 13:39:35 +0300

    added quotation marks at the end

--- RD_MySQL_Lesson02_hw/dumps_creating.sql
+++ RD_MySQL_Lesson02_hw/dumps_creating.sql
@@ -5,4 +5,4 @@ anon3131@anon3131-VirtualBox:~$ mysqldump example < exampledump.sql
 anon3131@anon3131-VirtualBox:~$ mysql sample < exampledump.sql
 
 Дамп первых 100 строк таблицы help_keyword базы данных mysql :
-anon3131@anon3131-VirtualBox:~$ mysqldump --opt --where='1 limit 100' mysql help_keyword > 100firstrows-help_keyword-mysqldump.sql
+anon3131@anon3131-VirtualBox:~$ mysqldump --opt --where='1 limit 100' mysql help_keyword > 100firstrows-help_keyword-mysqldump.sql";
