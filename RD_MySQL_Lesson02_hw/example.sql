c818e3fca672de14381e7177ab65ad68a4aca70d
Guro <demensdeo@gmail.com>
2022-01-02 22:51:32 +0300

    added improvements

--- RD_MySQL_Lesson02_hw/example.sql
+++ RD_MySQL_Lesson02_hw/example.sql
@@ -3,14 +3,15 @@ CREATE DATABASE example;
 USE example;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
- id smallint unsigned not null,
- name VARCHAR(255) DEFAULT 'username',
+ id SERIAL,
+ name VARCHAR(255) DEFAULT 'Anonymous',
  registration_date DATETIME DEFAULT CURRENT_TIMESTAMP
 ) COMMENT =  'Registred Users';
 SHOW tables;
 DESCRIBE users;
-INSERT INTO users (id, name) VALUES (1, 'Anon1');
-INSERT INTO users (id, name) VALUES (2, 'Anon2');
-INSERT INTO users (id, name) VALUES (3, 'Anon3');
+INSERT INTO users (name) VALUES ('Anon1');
+INSERT INTO users (name) VALUES ('Anon2');
+INSERT INTO users (name) VALUES ('Anon3');
 SELECT * FROM users;
 -- mysqldump example > exampledump.sql
+
