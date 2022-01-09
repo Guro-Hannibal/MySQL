747ec93f4dc843dff74bd2dc9773df100ae02479
Guro <demensdeo@gmail.com>
2022-01-09 04:12:53 +0300

    Table copy and addition

---
+++ Test_Work_DB/table_copy_and_addition.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE `new_procedure` ()
+BEGIN
+	DROP TABLE IF EXISTS demo;
+	CREATE TABLE demo LIKE employees;
+	INSERT INTO demo VALUES
+		(1, 'Noname',  'Suffering', NOW(), 'F', 5435 , 5345, 34534),
+		(2, 'Anon',  'Existence', NOW(), 'F', 5435 , 54353, 435),
+		(3, 'ThereIsNothingHere',  'GoForwardBeHappy', NOW(), 'F', 5345 , 34534, 34534),
+		(4, 'Empty',  'VeniVidiVici', NOW(), 'F', 1000 , 100, 200),
+		(5, 'PainIsMyBestAndClosestFriend',  'Win', NOW(), 'F', 34534 , 34534, 34534);
+END
