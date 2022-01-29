3ff56915888d4df3c20c0afe1519a23b7a2c39e3
Gurgen <demensdeo@gmail.com>
2022-01-29 07:33:15 +0300

    completed exercises 2-4

--- RD_MySQL_Lesson07_hw/hw7.sql
+++ RD_MySQL_Lesson07_hw/hw7.sql
@@ -7,3 +7,14 @@ ON l.user_id = p.user_id AND gender = 'F' OR gender = 'M'
 
 
 
+SELECT count(*) FROM likes WHERE user_id = (SELECT user_id FROM users);
+
+SELECT COUNT(l.user_id) AS given_likes, COUNT(l.target_id) AS getted_likes, u.id 
+FROM likes l 
+JOIN users u 
+ON u.id = l.user_id OR u.id = l.target_id
+
+
+
+
+
